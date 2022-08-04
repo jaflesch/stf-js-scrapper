b199df9aeea101ce8638b198de5ca7251fb25326
jaflesch <jean.flesch93@gmail.com>
2022-08-04 19:59:20 -0300

    wip: Yargs handler

--- pdf.js
+++ pdf.js
@@ -1,4 +1,27 @@
+const yargs = require('yargs/yargs')
+const colors = require('colors');
 const pdfController = require('./pdfController');
+
+// to do: available args
+	/**
+	 * @param offset: number 	> initial page to scrap
+	 * @param limit: 	number 	> total pages to scrap
+	 * @param size: 	number	> results per page
+	 * @param sort: 	string	> sort order <(new|old|relevant)(asc|desc)>
+	 * @param async: 	boolean	> pdf async downloads
+	 * @param debug: 	boolean	> log entries
+	*/
+const args = yargs(process.argv.slice(2))
+.options({
+	's': {
+		alias: 'size'.yellow,
+		demandOption: true,
+		default: 10,
+		describe: 'Resultados por página'.blue,
+		type: 'number'
+	}
+})
+.argv;
 
 (async function() {
 	const urls = await pdfController.getUrls();
