a9735206b7056907b858c812fdd9c7017023d50e
jaflesch <jean.flesch93@gmail.com>
2022-08-04 21:47:28 -0300

    Add command options

--- pdf.js
+++ pdf.js
@@ -2,23 +2,49 @@ const yargs = require('yargs/yargs')
 const colors = require('colors');
 const pdfController = require('./pdfController');
 
-// to do: available args
-	/**
-	 * @param offset: number 	> initial page to scrap
-	 * @param limit: 	number 	> total pages to scrap
-	 * @param size: 	number	> results per page
-	 * @param sort: 	string	> sort order <(new|old|relevant)(asc|desc)>
-	 * @param async: 	boolean	> pdf async downloads
-	 * @param debug: 	boolean	> log entries
-	*/
 const args = yargs(process.argv.slice(2))
 .options({
-	's': {
-		alias: 'size'.yellow,
+	'l': {
+		alias: 'limit'.yellow,
+		demandOption: false,
+		default: 0,
+		describe: 'Total de páginas'.blue,
+		type: 'number'
+	},
+	'o': {
+		alias: 'offset'.yellow,
+		demandOption: false,
+		default: 1,
+		describe: 'Página inicial'.blue,
+		type: 'number'
+	},
+	'r': {
+		alias: 'rows'.yellow,
 		demandOption: true,
 		default: 10,
 		describe: 'Resultados por página'.blue,
 		type: 'number'
+	},
+	'd': {
+		alias: 'debug'.yellow,
+		demandOption: false,
+		default: false,
+		describe: 'Log de eventos'.blue,
+		type: 'boolean'
+	},
+	'a': {
+		alias: 'async'.yellow,
+		demandOption: false,
+		default: false,
+		describe: 'Fazer download assíncronos'.blue,
+		type: 'boolean'
+	},
+	's': {
+		alias: 'sort'.yellow,
+		demandOption: false,
+		default: 10,
+		describe: 'Ordenação dos resultados. Segue o formato <(new|old|relevant)(asc|desc)>'.blue,
+		type: 'string'
 	}
 })
 .argv;
