5b5b9640949986e6152b172aa2d6afde1e327b8d
Jean Flesch <jean.flesch93@gmail.com>
2022-08-09 19:59:24 -0300

    Undo case-sensitive filename delete

---
+++ src/PageController.ts
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const colors = require('colors');
+import { Browser } from 'puppeteer';
+import { PageScraper } from './PageScraper';
+import { args } from './args';
+import { FileController } from './FileController';
+
+export class PageController {
+	constructor(
+		private readonly browserInstance: Promise<Browser | null>
+	){}
+
+	async execute ()  {
+		try {
+			const browser = await this.browserInstance;
+			if(! browser) {
+				throw `Chromium browser launching failed`.red;
+			}
+			const pageScraper = new PageScraper(browser, args);
+			const scrapedData = await pageScraper.run();	
+			const fileController = new FileController(scrapedData);
+			fileController.save();
+			await browser.close();
+		} 
+		catch (err) {
+			console.error(`Fatal error on execution: ${err}`.red);
+		}
+	}
+}
