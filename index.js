34f974819827c6501b781f5b1346e26d8f8534de
Jean Flesch <jean.flesch93@gmail.com>
2022-08-09 19:07:19 -0300

    Migrate to Typescript environment

--- index.js
+++
@@ -1,5 +0,0 @@
-const browserObject = require('./browser');
-const scraperController = require('./pageController');
-
-let browserInstance = browserObject.startBrowser();
-scraperController(browserInstance);
