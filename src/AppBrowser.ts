96f1edb65d7f92c5a4064f762b863a0411b5a734
Jean Flesch <jean.flesch93@gmail.com>
2022-08-10 00:42:30 -0300

    Remove logs

--- src/AppBrowser.ts
+++ src/AppBrowser.ts
@@ -12,20 +12,18 @@ export class AppBrowser {
 	async launch (): Promise<void> {
 		try {
 		  console.log("Launching Chromium browser......".blue);
-			var a = await puppeteer.launch({
+			
+			this.core = await puppeteer.launch({
 				headless: false,
-					'ignoreHTTPSErrors': true,
+				ignoreHTTPSErrors: true,
 				args: ["--disable-setuid-sandbox", "--start-maximized"],
 			});
-			console.log('@@@@', a);
-			this.core = a;
 		} catch (err) {
 	    console.log(`Could not create browser instance ${err}`);
 		}
 	}
 
 	async getBrowserInstance (): Promise<Browser | null> {
-		console.log('chamou esse cara', this.core);
 		return this.core;
 	}
 }
