ca69e31e7ab69123b4717210b756a8df30cf60a7
Jean Flesch <jean.flesch93@gmail.com>
2022-08-11 23:35:49 -0300

    wip:Save config file for paginated json option

--- src/FileController.ts
+++ src/FileController.ts
@@ -1,10 +1,15 @@
-import { CLIArgs } from "./args";
-
 const fs = require('fs');
+const colors = require('colors');
+import { CLIArgs } from "./args";
 
 type FileOptions = {
 	minify?: CLIArgs['minify'];
 	paginate?: CLIArgs['paginate'];
+}
+
+type Feedback = {
+	success: boolean;
+	message: string;
 }
 
 export class FileController {
@@ -14,9 +19,14 @@ export class FileController {
 	constructor(
 		private readonly rawData: any[],
 		private readonly _options?: FileOptions
-	){
+	) {
 		this.minified = !!(_options?.minify);
 		this.paginated = !!(_options?.paginate);
+	}
+
+	private createFileName () {
+		const now = new Date();
+		return `${now.getFullYear()}-${String(now.getMonth() + 1).padStart(2, '0')}-${String(now.getDate()).padStart(2, '0')}`;
 	}
 
 	async save ()  {
@@ -36,35 +46,93 @@ export class FileController {
 			*/
 			
 			if (this.paginated) {
+				let feedback: Feedback = {
+					success: false,
+					message: '',
+				};
+
 				for (let i = 0; i < this.rawData.length; i++) {
-					const folderPath = `${dir}/${i + 1}`;
+					const folderPath = `${dir}/${this.rawData[i].page}`;
 					if (! fs.existsSync(folderPath)) {
 						fs.mkdirSync(folderPath);
 					}
-
-					const now = new Date();			
-					const fileName = `${now.getFullYear()}${now.getMonth() + 1}${now.getDate()}`;
-
+					
 					fs.writeFile(
-						`${folderPath}/${fileName}.json`, 
+						`${folderPath}/data-${this.createFileName()}.json`, 
 						JSON.stringify(this.rawData[i].data, undefined, this.minified ? undefined : 2), 
 						'utf8', 
-						function(err: Error) {
+						(err: Error) => {
 							if(err) {
-								return console.log(err);
+								feedback = {
+									success: false,
+									message: err.message,
+								}
+								return false;								
 							}
-							console.log("The data has been scrapped and saved successfully! View it at './json/'".green);
+							feedback = {
+								success: true,
+								message: "The data has been scrapped and saved successfully! View it at './json/'".green,
+							}
 						}
 					);
 				}
+
+				if (feedback) {
+					const feedbackColor = feedback.success ? 'green' : 'red';
+					console.log(colors[feedbackColor](feedback.message));
+				}
+
+				// Config file:
+				const options = this.rawData[0].options;
+				const startAt = this.rawData[0].start;
+				const endAt = this.rawData[this.rawData.length - 1].end;
+
+				fs.writeFile(
+					`${dir}/config-${this.createFileName()}.json`, 
+					JSON.stringify({
+						startAt: new Date(startAt),
+						endAt: new Date(endAt),
+						executionTime: Math.floor((endAt - startAt) / 1000),
+						...options,
+					}, undefined, this.minified ? undefined : 2), 
+					'utf8', 
+					(err: Error) => {
+						if(err) {
+							feedback = {
+								success: false,
+								message: err.message,
+							}
+							return false;								
+						}
+						feedback = {
+							success: true,
+							message: "Query params configuration file was created at './json'".green,
+						}
+					}
+				);
+
 			} else {
+
 				fs.writeFile(
 					`${dir}/data.json`, 
 					JSON.stringify(this.rawData, undefined, this.minified ? undefined : 2),
 					'utf8', 
 					function(err: Error) {
 						if(err) {
-							return console.log(err);
+							console.log(`Error when saving file: ${err}`.red);
+							return false;
+						}
+						console.log("The data has been scrapped and saved successfully! View it at './json/data.json'".green);
+					}
+				);
+				fs.writeFile(
+					`${dir}/query-params.json`, 
+					JSON.stringify(this.rawData, undefined, this.minified ? undefined : 2),
+					'utf8', 
+					function(err: Error) {
+						if(err) {
+							console.log(`Error when saving file: ${err}`.red);
+							return false;
 						}
 						console.log("The data has been scrapped and saved successfully! View it at './json/data.json'".green);
 					}
