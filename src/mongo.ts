f3bd200964a2c27d8cffbfadd5181063c4171a68
Jean Flesch <jean.flesch93@gmail.com>
2022-09-13 00:05:55 -0300

    Improve collection insert validation performance

--- src/mongo.ts
+++ src/mongo.ts
@@ -2,11 +2,12 @@ const fs = require('fs');
 const path = require('path');
 import 'dotenv/config';
 import mongoose from "mongoose";
+import { exit } from 'process';
 import { Judgement } from "./models/judgement.model";
 import { JSONtoModel } from "./query-builder/utils";
 
 main()
-  .then(() => console.log('Database succesfully connected!'))
+  .then(() => exit())
   .catch(err => console.log(err));
 
 async function main() {
@@ -18,23 +19,29 @@ async function main() {
   let acordaos: any = [];
   acordaos = acordaos.concat(...JSON.parse(jsonData));
   
-  con.on('disconnected', () => console.log('Disconnected successfully!'));
+  const judgementIds = new Set([
+    ...(
+      await Judgement.find().select('id').exec()
+    ).map(j => j.id)
+  ]);
+  
   let count = 0;
+  let repeated = 0;
   for (const acordao of acordaos) {
+    if (judgementIds.has(acordao.id)) {
+      repeated++;
+      continue;
+    }
+
     const row = new Judgement(
       JSONtoModel(acordao)
-    );    
+    );       
     
-    const alreadyExistsJudgement = await Judgement.find({ 
-      id: row.id 
-    }).exec();
-
-    if (alreadyExistsJudgement.length === 0) {
-      const result = await row.save();
-      console.log('Inserted row with ID: ', result?._id.valueOf());
-      count++;
-    }
+    const result = await row.save();
+    console.log('Inserted row with ID: ', result?._id.valueOf());
+    count++;
   }
+  con.close();
 
   console.log(`### Finished with: [${count}/${acordaos.length}] insertions.`);
 }
