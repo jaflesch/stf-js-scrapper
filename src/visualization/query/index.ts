a6cb86f9e868f33e753eeb9a199cac46570f9718
Jean Flesch <jean.flesch93@gmail.com>
2022-09-20 23:55:21 -0300

    Add judicial body by year query

--- src/visualization/query/index.ts
+++ src/visualization/query/index.ts
@@ -1,3 +1,4 @@
 export { JudgementsByYearQuery } from './judgement-by-year.query';
+export { JudicialBodyByYearQuery } from './judicial-body-by-year.query';
 export { JudgementsByWriterQuery } from './judgements-by-writer.query';
 export { JudgementsByRappoteurQuery } from './judgements-by-rappoteur.query';
