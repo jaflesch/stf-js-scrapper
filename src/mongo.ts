aabb02adcd4b2f2c57b8c6caab46bb8e029571f4
Jean Flesch <jean.flesch93@gmail.com>
2022-09-25 20:25:26 -0300

    Add batch merge files logic for database insertion

--- src/mongo.ts
+++ src/mongo.ts
@@ -1,6 +1,6 @@
-const fs = require('fs');
-const path = require('path');
 import 'dotenv/config';
+import fs from 'fs';
+import path  from 'path';
 import mongoose from "mongoose";
 import { exit } from 'process';
 import { Judgement } from "./models/judgement.model";
@@ -14,34 +14,42 @@ async function main() {
   const db = await mongoose.connect(`mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_TABLENAME}`);
   const con = db.connection;
 
-  const jsonFilePath = '../db/__2010.merge.json';
-  const jsonData = fs.readFileSync(path.join(__dirname, jsonFilePath));
-  let acordaos: any = [];
-  acordaos = acordaos.concat(...JSON.parse(jsonData));
-  
-  const judgementIds = new Set([
-    ...(
-      await Judgement.find().select('id').exec()
-    ).map(j => j.id)
-  ]);
+  const mergeFilesFolder = fs.readdirSync(path.join(__dirname, '../db/'));
+  const mergeFiles = mergeFilesFolder.filter(mf => mf.match(/\.merge\.json$/))
   
   let count = 0;
   let repeated = 0;
-  for (const acordao of acordaos) {
-    if (judgementIds.has(acordao.id)) {
-      repeated++;
-      continue;
-    }
 
-    const row = new Judgement(
-      JSONtoModel(acordao)
-    );       
+  for (const mergeFile of mergeFiles) {
+    let acordaos: any = [];
+    const jsonData = fs.readFileSync(path.join(__dirname, '../db/', mergeFile), 'utf-8');
     
-    const result = await row.save();
-    console.log('Inserted row with ID: ', result?._id.valueOf());
-    count++;
+    acordaos = acordaos.concat(...JSON.parse(jsonData));
+    
+    const judgementIds = new Set([
+      ...(
+        await Judgement.find().select('id').exec()
+      ).map(j => j.id)
+    ]);
+        
+    for (const acordao of acordaos) {
+      if (judgementIds.has(acordao.id)) {
+        repeated++;
+        continue;
+      }
+  
+      const row = new Judgement(
+        JSONtoModel(acordao)
+      );
+      
+      const result = await row.save();
+      console.log('Inserted row with ID: ', result?._id.valueOf());
+      count++;
+
+      //if(count > 1) break;
+    }
   }
   con.close();
 
-  console.log(`### Finished with: [${count}/${acordaos.length}] insertions.`);
+  console.log(`### Finished with: [${count}/${count + repeated}] insertions.`);
 }
