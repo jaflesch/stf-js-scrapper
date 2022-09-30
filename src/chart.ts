54b32408aae442a1b096dee0f9228f2a45fe2233
Jean Flesch <jean.flesch93@gmail.com>
2022-09-29 23:37:10 -0300

    Add judgements by document category count query

--- src/chart.ts
+++ src/chart.ts
@@ -1,4 +1,7 @@
-import path from 'path';
+import * as cherrio from "cheerio";
+import fs from 'fs';
+import  pdf from 'pdf-parse';
+import path, { dirname } from 'path';
 import express, {Request, Response} from 'express';
 import { normalizeAverageValue } from './visualization/infra/normalize-avg-value';
 import { 
@@ -8,7 +11,10 @@ import {
   JudicialBodyByYearQuery, 
   JudgementsByRappoteurQuery,
   JudgementsByLocationCountQuery,
+  JudgementsByDocumentCategoryCountQuery,
 } from './visualization/query';
+import { removeStopwords, porBr } from 'stopword';
+const pdf2html = require('pdf2html');
 
 const app = express();
 const router = express.Router();
@@ -116,6 +122,108 @@ router.get('/votos-local', async ({ query }: Request, res: Response) => {
     chartTitle: 'Total acõrdãos / Local',
     chart: JSON.stringify(chart),
   });
+});
+
+router.get('/acordao-categoria', async ({ query }: Request, res: Response) => {
+  const judgementsByDocumentCategoryCountQuery = new JudgementsByDocumentCategoryCountQuery();
+  const chart = await judgementsByDocumentCategoryCountQuery.execute(query);
+
+  res.render("acordao-categoria", { 
+    chartTitle: 'Total acõrdãos / Categoria',
+    chart: JSON.stringify(chart),
+  });
+});
+
+router.get('/pdf', async (req: Request, res: Response) => {
+  const f = __dirname + '/acordao.pdf'
+  let dataBuffer = fs.readFileSync(f);
+  pdf2html.html(f, (err:any, html:any) => {
+    if (err) {
+      console.error('Conversion error: ' + err)
+    } else {
+      const $ = cherrio.load(html);
+      let output = '';
+      $('p').each(function(i, el) {
+        const text = $(el).text().split(' ');
+        output += `${removeStopwords(text, porBr).join(' ').replace(',', ' ').replace('.', ' ').toLocaleLowerCase()}\n`;
+      })
+      
+      res.render('pdf', {
+        text: output
+      })
+    }
+  });
+
+  pdf2html.meta(f, (err: any, meta: any) => {
+    if (err) {
+        console.error('Conversion error: ' + err)
+    } else {
+      console.log(meta)
+      /*
+        'pdf:unmappedUnicodeCharsPerPage': [
+          '0', '0', '0', '0', '0',
+          '0', '0', '0', '0', '0',
+          '0', '0', '0', '0', '0',
+          '0', '0', '0', '0', '0',
+          '0', '0', '0', '0'
+        ],
+        'pdf:PDFVersion': '1.4',
+        'pdf:hasXFA': 'false',
+        'access_permission:modify_annotations': 'true',
+        'access_permission:can_print_degraded': 'true',
+        'dcterms:created': '2022-09-26T19:50:58Z',
+        'dcterms:modified': '2022-09-26T19:50:58Z',
+        'dc:format': 'application/pdf; version=1.4',
+        'access_permission:fill_in_form': 'true',
+        'pdf:docinfo:modified': '2022-09-26T19:50:58Z',
+        'pdf:hasCollection': 'false',
+        'pdf:encrypted': 'false',
+        'Content-Length': '519502',
+        'pdf:hasMarkedContent': 'false',
+        'Content-Type': 'application/pdf',
+        'pdf:producer': 'iText 2.1.5 (by lowagie.com)',
+        'access_permission:extract_for_accessibility': 'true',
+        'access_permission:assemble_document': 'true',
+        'xmpTPg:NPages': '24',
+        resourceName: 'acordao.pdf',
+        'pdf:hasXMP': 'false',
+        'pdf:charsPerPage': [
+          '1601', '3539', '3851',
+          '3596', '4178', '4546',
+          '4482', '4688', '5138',
+          '5257', '5455', '5415',
+          '5395', '5195', '5375',
+          '5249', '4605', '4283',
+          '4829', '4547', '5019',
+          '4813', '1415', '2602'
+        ],
+        'access_permission:extract_content': 'true',
+        'access_permission:can_print': 'true',
+        'X-TIKA:Parsed-By': [
+          'org.apache.tika.parser.DefaultParser',
+          'org.apache.tika.parser.pdf.PDFParser'
+        ],
+        'access_permission:can_modify': 'true',
+        'pdf:docinfo:producer': 'iText 2.1.5 (by lowagie.com)',
+        'pdf:docinfo:created': '2022-09-26T19:50:58Z'
+      */
+    }
+})
+
+
+  return; 
+  pdf(dataBuffer).then(function(data: any) {
+    // use data
+    console.log(data.numpages);
+    console.log(data.numrender);
+    console.log(data.info);
+    console.log(data.metadata); 
+    console.log(data.version);
+    
+    res.render('pdf', {
+      text: data.text
+    })
+  })  
 });
 
 app.use('/', router);
---
+++ src/visualization/query/judgements-by-document-category-count.query.ts
@@ -0,0 +1,95 @@
+import { Query } from "../domain/query";
+import { Judgement } from "../../models/judgement.model";
+import { QueryBuilder } from "../../query-builder";
+import { DocumentCategory } from "../infra/document-category.enum";
+import { MongoDBPipeline } from "../infra/mongodb-pipeline.type";
+import { documentCategoryToLabelValue } from "../infra/translate-document-category";
+
+type Params = {
+  startAt?: number;
+  endAt?: number;
+  category?: DocumentCategory;
+}
+
+type ResultDTO = Array<{
+  count: number;
+  category: string;
+}>
+
+export class JudgementsByDocumentCategoryCountQuery implements Query<Params, ResultDTO> {
+  public async execute(queryParams: Params): Promise<ResultDTO> {
+    const match = this.mountQueryParams(queryParams);
+    const qb = new QueryBuilder(Judgement).$();
+
+    const result = await qb.aggregate([
+      {
+        $addFields: {
+          year: {
+            $year: '$dataJulgamento',
+          },
+        },
+      },
+      { 
+        $match: { 
+          ...match 
+        }
+      },
+      {
+        $group: {
+          _id: {
+            categoria: "$categoria"
+          },
+          count: {
+            $sum: 1
+          }
+        }
+      },
+      {
+        $project: {
+          category: "$_id.categoria",
+          count: "$count",
+          _id: 0
+        }
+      },
+      {
+        $sort: {
+          count: -1
+        }
+      }
+    ]);
+    
+    return result.map(r => ({
+      ...r,
+      categoryLabel: documentCategoryToLabelValue(r.category)
+    }));
+  }
+
+  private mountQueryParams (queryParams: Params): MongoDBPipeline {
+    const match: MongoDBPipeline['match'] = {};
+    const yearMatch = [];
+
+    if (queryParams.startAt) {
+      yearMatch.push({ 
+        year: { 
+          $gte: Number(queryParams.startAt) 
+        } 
+      });
+    }
+    if (queryParams.endAt) {
+      yearMatch.push({
+        year: {
+          $lte: Number(queryParams.endAt)
+        }
+      });
+    }
+    if (queryParams.category) {
+      match.categoria = queryParams.category;
+    }
+
+    if (yearMatch.length > 0) {
+      match.$and = yearMatch;
+    } 
+    
+    return match;
+  }
+}
