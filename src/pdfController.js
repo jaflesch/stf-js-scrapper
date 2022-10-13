5833e4fc993f5dfe339618235737e3c6662259b2
Jean Flesch <jean.flesch93@gmail.com>
2022-10-12 23:50:19 -0300

    wip: download pdf service

--- src/pdfController.js
+++ src/pdfController.js
@@ -1,25 +1,34 @@
 const fs = require('fs');
 const colors = require('colors');
 const pdfDownloader = require('./pdfDownloader');
+const { Judgement } = require('./models/judgement.model');
+const { QueryBuilder } = require('./query-builder');
 
 async function getUrls () {
 	try {
-		const json = fs.readFileSync("json/data.json", 'utf8');
-		const fileData = JSON.parse(json);
+		const qb = new QueryBuilder(Judgement).$();
+		const result = await qb.find().select({
+				id: 1, 
+				_id: 0,
+				arquivoPdfUrl: 1, 
+				documentId: 1,
+		});
 		
-		return fileData[0].data.map(record => record.pdfFileUrl);		
+		//console.log(r)
+		return result.map(record => record.arquivoPdfUrl);
 	} catch(err) {
-		console.error("Could not open specified file".red, err);
+		console.error(`Could not fetch PDF url list: ${err.message}`.red);
 	}
 }
 
 async function download (urls) {
 	try {
 		const pd  = await pdfDownloader.start(urls);	
+		pdfDownloader.createLog(pd);
 		console.log(`${pd}`.magenta);
 		
 	} catch(err) {
-		console.error("An error occurred when downloading pdf files".red, err);
+		console.error(`An error occurred when downloading pdf files: ${err.message}`.red);
 	}
 }
 
--- src/pdfDownloader.js
+++ src/pdfDownloader.js
@@ -1,35 +1,74 @@
 const { DownloaderHelper } = require('node-downloader-helper');
 const colors = require('colors');
+import { existsSync, mkdirSync, writeFile } from 'fs';
+import path from 'path';
 
 const pdfDownloader = {
-	async start(urls, syncDownload = true) {
+	async start(urls, syncDownload = true, destFolder = '../pdf') {
 		console.log('..::PDF Scraper::..'.blue);
+		console.log(`${urls.length} urls found\n`.blue);
 
-		let responses = [];
-		for (let i = 0; i < urls.length; i++) {
-			console.warn(`[${i + 1}] Downloading from ${urls[i]} ...`.yellow);
-			const fileName = urls[i].split('&docID=')[1] + '.pdf';
-			const download = new DownloaderHelper(urls[i], `${__dirname}/pdf`, { fileName });
-			
-			download.on('end', () => console.log('Download completed!\n'.green))
-			download.on('error', (err) => console.log('Download failed'.red, err));
+		const responses = [];
+		const destinationFolder = path.join(__dirname, destFolder);	
+		let currentUrl = '', fileName = '', status = 0;
+		
+		try {
 
-			try {
-				if (syncDownload) {
+			if (! existsSync(destinationFolder)) {
+				mkdirSync(destinationFolder);
+			}
+
+			for (let i = 0; i < urls.length; i++) {
+				currentUrl = urls[i];
+				console.warn(`[${i + 1}] Downloading file from: ${urls[i]} ...`.yellow);
+				fileName = currentUrl.split('&docID=')[1] + '.pdf';
+							
+				const download = new DownloaderHelper(
+					urls[i], 
+					destinationFolder, 
+					{ fileName }
+				);					
+				download.on('end', () => console.log('Download completed!\n'.green))
+				download.on('error', (err) => console.log('Download failed: '.red, err.status));
+				
+				let status = 0;
+				try {
 					await download.start();
-				} else {
-					download.start();
+				} catch (err) {
+					status = err.status;
 				}
-			} catch (err) {}
-			
-			responses.push({
-				fileName,
-				size: download.getTotalSize()
-			});
+
+				responses.push({
+					fileName,
+					size: status ? download.getTotalSize() : 0,
+					error: status,
+				});
+			}
+		}	catch (err) {
+			console.log('@@@@@', err);
 		}
+		
+		writeFile(
+			path.join(destinationFolder, 'log.json'),
+			JSON.stringify(responses, undefined, 2), 
+			'utf8', 
+			(err) => {
+				if(err) {
+					feedback = {
+						success: false,
+						message: err.message,
+					}
+					console.log(`Error when creating logfile: ${err.message}`.red);					
+				}
+
+				console.log(`The data has been scrapped and saved successfully! View it at '${destinationFolder}'`.green);
+			}
+		);
 
 		return responses;
 	}
+
+	
 }
 
 module.exports = pdfDownloader;
