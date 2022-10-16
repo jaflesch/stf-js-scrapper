2d8ba51c5a66641cc32556fefa012b68ed6bef1a
Jean Flesch <jean.flesch93@gmail.com>
2022-10-15 23:18:54 -0300

    fix: memory leak @ pdf convertion pipeline

--- src/Pdf2TextController.ts
+++ src/Pdf2TextController.ts
@@ -1,27 +1,44 @@
-import * as cherrio from "cheerio";
-import path, { dirname } from 'path';
+import path from 'path';
 import colors from 'colors';
-const pdf2html = require('pdf2html');
+import { readFileSync, writeFileSync } from "fs";
+import PdfParse from "pdf-parse";
 
 export class Pdf2TextController {
   constructor(
-    private readonly file: string,
+    private readonly files: string[],
+    private readonly srcFolder: string,
+    private readonly dstFolder: string,
   ) {}
 
-  async execute(srcFolder: string, cb?: Function) {    
-    pdf2html.html(path.join(srcFolder, this.file), (err:any, html:any) => {
-      if (err) {
+  async execute() {    
+    console.log(colors.yellow('Starting...'));
+    for (let i = 0; i < this.files.length; i++) {
+      const fileName = `${this.files[i].substring(0, this.files[i].length - 4)}.txt`;
+      console.log(colors.yellow(`[${i + 1}] Converting '${this.files[i]} to text...`));
+
+      const dataBuffer = readFileSync(path.join(this.srcFolder, this.files[i]));
+      try {
+        const { text } = await PdfParse(dataBuffer);
+        this.createTextFile(fileName, text.trim());
+
+      } catch (err: any) {
         console.error(colors.red(`Error on PDF to HTML conversion: ${err.message}`));
-      } else {
-        const $ = cherrio.load(html);
-        
-        let output = '';          
-        $('p').each(function(i, el) {
-          output += $(el).text() + '\n';
-        });
-        
-        if (cb) cb(output.trim());
       }
-    });
+    }
+  } 
+
+  createTextFile (fileName: string, text: string) {
+    try {
+      writeFileSync(
+        path.join(this.dstFolder, fileName), 
+        text, 
+        'utf8'
+      );
+      console.log(colors.green(`File '${fileName}' successfuly saved at '${this.dstFolder}'\n`));
+    } catch (err: any) {
+      if(err) {
+        console.log(colors.red(`Error when creating file '${fileName}': ${err.message}\n`));					
+      }
+    }
   }
 }
